221efe01a70d70c5229bd3733f1cc3b25250a38b
Marl <winering@gmail.com>
2024-02-15 00:04:02 -0300

    Commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export const App = () => {
     <BrowserRouter>
       <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom-dark">
         <Link
-          to="/"
+          to="/peliculas-app"
           className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
         >
           <h1 className="title">Peliculas</h1>
